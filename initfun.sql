712bfc7afdc96d428e65577c3b586cfd51f17bd8
Barry Deeney <mxaddict@codedmaster.com>
2018-02-15 16:08:59 +0800

    Update schema to match required changes for the product and owner relationship

--- initfun.sql
+++ initfun.sql
@@ -27,22 +27,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `brands` (
-  `brand_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `brand_name` varchar(255) NOT NULL,
-  `brand_active` int(11) NOT NULL DEFAULT '0',
-  `brand_status` int(11) NOT NULL DEFAULT '0'
+  `brand_active` int(1) NOT NULL DEFAULT '0',
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `brands`
 --
 
-INSERT INTO `brands` (`brand_id`, `brand_name`, `brand_active`, `brand_status`) VALUES
-(1, 'No brand', 1, 1),
-(2, 'Slice bread brand', 1, 1),
-(3, 'Otap brand', 1, 1),
-(4, 'Shamrock', 1, 1),
-(5, 'Max', 1, 1);
+INSERT INTO `brands` (`id`, `brand_name`, `brand_active`) VALUES
+(1, 'No brand', 1),
+(2, 'Shamrock', 1),
+(3, 'Max', 1);
 
 -- --------------------------------------------------------
 
@@ -51,24 +48,23 @@ INSERT INTO `brands` (`brand_id`, `brand_name`, `brand_active`, `brand_status`) 
 --
 
 CREATE TABLE `categories` (
-  `categories_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `categories_name` varchar(255) NOT NULL,
-  `categories_active` int(11) NOT NULL DEFAULT '0',
-  `categories_status` int(11) NOT NULL DEFAULT '0'
+  `categories_active` int(1) NOT NULL DEFAULT '0',
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`categories_id`, `categories_name`, `categories_active`, `categories_status`) VALUES
-(1, 'Bread', 1, 1),
-(2, 'Cake', 1, 1),
-(3, 'Cake Rolls', 1, 1),
-(4, 'Cookies', 1, 1),
-(5, 'Pastries', 2, 1),
-(6, 'Pasalubongs', 1, 1),
-(7, 'Candy', 1, 1);
+INSERT INTO `categories` (`id`, `categories_name`, `categories_active`) VALUES
+(1, 'Bread', 1),
+(2, 'Cake', 1),
+(3, 'Cake Rolls', 1),
+(4, 'Cookies', 1),
+(5, 'Pastries', 2),
+(6, 'Pasalubongs', 1),
+(7, 'Candy', 1);
 
 -- --------------------------------------------------------
 
@@ -106,7 +102,7 @@ INSERT INTO `customers` (`id`, `first_name`, `middle_name`, `last_name`, `gender
 --
 
 CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `order_date` date NOT NULL,
   `client_name` varchar(255) NOT NULL,
   `client_contact` varchar(255) NOT NULL,
@@ -126,7 +122,7 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`order_id`, `order_date`, `client_name`, `client_contact`, `sub_total`, `vat`, `total_amount`, `discount`, `grand_total`, `paid`, `due`, `payment_type`, `payment_status`, `order_status`) VALUES
+INSERT INTO `orders` (`id`, `order_date`, `client_name`, `client_contact`, `sub_total`, `vat`, `total_amount`, `discount`, `grand_total`, `paid`, `due`, `payment_type`, `payment_status`, `order_status`) VALUES
 (1, '2018-01-12', 'Richly Kwong', '0909090909', '110.00', '14.30', '124.30', '0', '124.30', '125', '-0.70', 2, 1, 1);
 
 -- --------------------------------------------------------
@@ -136,7 +132,7 @@ INSERT INTO `orders` (`order_id`, `order_date`, `client_name`, `client_contact`,
 --
 
 CREATE TABLE `order_item` (
-  `order_item_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL DEFAULT '0',
   `product_id` int(11) NOT NULL DEFAULT '0',
   `quantity` varchar(255) NOT NULL,
@@ -149,7 +145,7 @@ CREATE TABLE `order_item` (
 -- Dumping data for table `order_item`
 --
 
-INSERT INTO `order_item` (`order_item_id`, `order_id`, `product_id`, `quantity`, `rate`, `total`, `order_item_status`) VALUES
+INSERT INTO `order_item` (`id`, `order_id`, `product_id`, `quantity`, `rate`, `total`, `order_item_status`) VALUES
 (1, 1, 2, '22', '5', '110.00', 1);
 
 -- --------------------------------------------------------
@@ -159,31 +155,30 @@ INSERT INTO `order_item` (`order_item_id`, `order_id`, `product_id`, `quantity`,
 --
 
 CREATE TABLE `product` (
-  `product_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_image` text NOT NULL,
   `brand_id` int(11) NOT NULL,
   `categories_id` int(11) NOT NULL,
   `quantity` varchar(255) NOT NULL,
   `rate` varchar(255) NOT NULL,
-  `active` int(11) NOT NULL DEFAULT '0',
-  `status` int(11) NOT NULL DEFAULT '0'
+  `active` int(1) NOT NULL DEFAULT '0',
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`product_id`, `product_name`, `product_image`, `brand_id`, `categories_id`, `quantity`, `rate`, `active`, `status`) VALUES
-(2, 'Cheese Bread', '../assests/images/initfun/241785a6600a42a21e.jpg', 1, 1, '10', '12', 1, 1),
-(3, 'empanadas', '../assests/images/initfun/88995a6611cd28383.jpg', 1, 1, '10', '10', 1, 1),
-(4, 'Hopia', '../assests/images/initfun/226725a6630ad60a13.jpg', 1, 1, '10', '20', 1, 1),
-(5, 'Monay', '../assests/images/initfun/178015a6630c3973cc.jpg', 1, 1, '10', '5', 1, 1),
-(6, 'Pandesal', '../assests/images/initfun/24465a6630d909544.jpg', 1, 1, '40', '5', 1, 1),
-(7, 'Pinagong', '../assests/images/initfun/14555a6630f475132.jpg', 1, 1, '50', '10', 1, 1),
-(8, 'Cinnamon Roll', '../assests/images/initfun/281325a6771c31d23b.jpg', 1, 3, '40', '200', 1, 1),
-(9, 'Pound Cake', '../assests/images/initfun/292615a6771f4857ed.jpg', 1, 2, '29', '400', 1, 1),
-(10, 'Dried Mango', '../assests/images/initfun/207385a6772130ee45.jpg', 1, 6, '29', '7', 1, 1);
+INSERT INTO `product` (`id`, `product_name`, `product_image`, `brand_id`, `categories_id`, `quantity`, `rate`, `active`) VALUES
+(2, 'Cheese Bread', '../assests/images/initfun/241785a6600a42a21e.jpg', 1, 1, '10', '12', 1),
+(3, 'empanadas', '../assests/images/initfun/88995a6611cd28383.jpg', 1, 1, '10', '10', 1),
+(4, 'Hopia', '../assests/images/initfun/226725a6630ad60a13.jpg', 1, 1, '10', '20', 1),
+(5, 'Monay', '../assests/images/initfun/178015a6630c3973cc.jpg', 1, 1, '10', '5', 1),
+(6, 'Pandesal', '../assests/images/initfun/24465a6630d909544.jpg', 1, 1, '40', '5', 1),
+(7, 'Pinagong', '../assests/images/initfun/14555a6630f475132.jpg', 1, 1, '50', '10', 1),
+(8, 'Cinnamon Roll', '../assests/images/initfun/281325a6771c31d23b.jpg', 1, 3, '40', '200', 1),
+(9, 'Pound Cake', '../assests/images/initfun/292615a6771f4857ed.jpg', 1, 2, '29', '400', 1),
+(10, 'Dried Mango', '../assests/images/initfun/207385a6772130ee45.jpg', 1, 6, '29', '7', 1);
 
 -- --------------------------------------------------------
 
@@ -192,18 +187,22 @@ INSERT INTO `product` (`product_id`, `product_name`, `product_image`, `brand_id`
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `is_admin` int(1) NOT NULL DEFAULT 0,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `brand_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `password`, `email`) VALUES
-(1, 'initfunadmin', '200820e3227815ed1756a6b531e7e0d2', '');
+INSERT INTO `users` (`id`, `is_admin`, `username`, `password`, `email`, `brand_id`) VALUES
+(1, 1, 'initfunadmin', '200820e3227815ed1756a6b531e7e0d2', '', 1),
+(2, 0, 'shamrock', '200820e3227815ed1756a6b531e7e0d2', '', 2)
+(3, 0, 'max', '200820e3227815ed1756a6b531e7e0d2', '', 3);
 
 --
 -- Indexes for dumped tables
@@ -213,13 +212,13 @@ INSERT INTO `users` (`user_id`, `username`, `password`, `email`) VALUES
 -- Indexes for table `brands`
 --
 ALTER TABLE `brands`
-  ADD PRIMARY KEY (`brand_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`categories_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `customers`
@@ -231,25 +230,25 @@ ALTER TABLE `customers`
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `order_item`
 --
 ALTER TABLE `order_item`
-  ADD PRIMARY KEY (`order_item_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -259,12 +258,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `categories_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `customers`
 --
@@ -274,22 +273,22 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `order_item`
 --
 ALTER TABLE `order_item`
-  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
